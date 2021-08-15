ab0dbb7432aaf477e4851a72579dcd47e4b829e9
Edgar Lopez <edgarla.isc@gmail.com>
2021-08-15 20:34:22 +0200

    Added logout functionality

--- src/components/PasswordHealth/components/Header/Header.tsx
+++ src/components/PasswordHealth/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import {FC} from 'react';
+import { useHistory } from 'react-router-dom';
+import { Routes } from '~/constants';
 import {IItem} from "~/services/getUserItems";
 import logout from '../../../../services/logout';
 
@@ -10,11 +12,21 @@ interface IHeader {
 }
 
 const Header: FC<IHeader> = ({items, username}) => {
+  const {replace} = useHistory();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      replace(Routes.Root);
+    } catch(error) {
+      console.error(error.message);
+    }
+  }
 
   return (
     <div className="header">
       <div className="user-section">
-        <button onClick={logout}>{`Logout ${username}`}</button>
+        <button onClick={handleLogout}>{`Logout ${username}`}</button>
       </div>
       <h1>{`${items.length} Items are vulnerable`}</h1>
       <span>Create new complex passwords to protect your accounts</span>
--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,15 @@
-const logout = () => {
+import { API } from '~/constants';
+import getUrl from '../utils/getUrl';
 
+const logout = async () => {
+  const url = getUrl(API.Logout);
+
+  await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+  localStorage.removeItem('token');
 };
 
 export default logout;
